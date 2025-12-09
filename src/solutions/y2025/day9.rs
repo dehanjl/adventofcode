e5693951405acbbff3a30c05fff46655b661e8be
Dehan Jaco Lamprecht <dehanjl@pm.me>
2025-12-09 21:24:31 +0200

    2025 Day 9 Part 1

--- src/solutions/y2025/day9.rs
+++ src/solutions/y2025/day9.rs
@@ -1,23 +1,31 @@
-use crate::register_day;
+use itertools::Itertools;
 
-fn parse_input(input: &str) -> Vec<String> {
-// TODO: parse the input into a more useful structure
-input
-.lines()
-.map(|line| line.to_string())
-.collect()
+use crate::{register_day, utils::Loc};
+
+fn parse_input(input: &str) -> Vec<Loc> {
+    input
+        .lines()
+        .map(|line| sscanf::scanf!(line, "{},{}", isize, isize).unwrap())
+        .map(|(c, r)| Loc(r, c))
+        .collect()
 }
 
 fn part1(input: &str) {
-let _data = parse_input(input);
-// TODO: solve part 1
-println!("Day 9 Part 1: {}", "TODO");
+    let tiles = parse_input(input);
+
+    let area = tiles
+        .iter()
+        .tuple_combinations()
+        .map(|(loc1, loc2)| (loc1.0.abs_diff(loc2.0) + 1) * (loc1.1.abs_diff(loc2.1) + 1))
+        .max()
+        .unwrap();
+
+    println!("Day 9 Part 1: {}", area);
 }
 
 fn _part2(input: &str) {
-let _data = parse_input(input);
-// TODO: solve part 2
-println!("Day 9 Part 2: {}", "TODO");
+    let _data = parse_input(input);
+    println!("Day 9 Part 2: {}", "TODO");
 }
 
 register_day!(2025, 9, part1);
