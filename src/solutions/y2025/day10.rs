f5f971b6f0fa62e8f48d6399df4498448c8a5b07
Dehan Lamprecht <dehanjl@pm.me>
2025-12-10 17:11:20 +0200

    2025 Day 10 Part 2 (not working)
    TODO: use a priority queue and distance to target

--- src/solutions/y2025/day10.rs
+++ src/solutions/y2025/day10.rs
@@ -1,3 +1,5 @@
+use indicatif::ParallelProgressIterator;
+use indicatif::ProgressIterator;
 use std::collections::VecDeque;
 
 use rayon::iter::IntoParallelRefIterator;
@@ -5,9 +7,27 @@ use rayon::iter::ParallelIterator;
 
 use crate::register_day;
 
+struct Button {
+    positions: Vec<u32>,
+    value: u32,
+}
+
+impl Button {
+    fn new_from_str(s: &str) -> Self {
+        let positions = s
+            .trim_start_matches('(')
+            .trim_end_matches(')')
+            .split(',')
+            .map(|part| part.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>();
+        let value = from_bits(positions.clone());
+        Button { positions, value }
+    }
+}
+
 struct Machine {
     target: u32,
-    buttons: Vec<u32>,
+    buttons: Vec<Button>,
     joltages: Vec<u32>,
 }
 
@@ -26,15 +46,7 @@ impl Machine {
 
         let buttons = seqs[1..seqs.len() - 1]
             .iter()
-            .map(|btn_str| {
-                from_bits(
-                    btn_str
-                        .trim_start_matches('(')
-                        .trim_end_matches(')')
-                        .split(',')
-                        .map(|part| part.parse::<u32>().unwrap()),
-                )
-            })
+            .map(|btn_str| Button::new_from_str(btn_str))
             .collect::<Vec<_>>();
 
         let joltages = seqs[seqs.len() - 1]
@@ -84,7 +96,8 @@ fn part1(input: &str) {
         .par_iter()
         .map(|machine| {
             // state, presses
-            let mut q: VecDeque<(u32, u32)> = machine.buttons.iter().map(|&b| (b, 1)).collect();
+            let mut q: VecDeque<(u32, u32)> =
+                machine.buttons.iter().map(|b| (b.value, 1)).collect();
             loop {
                 let (state, presses) = q.pop_front().unwrap();
                 if state == machine.target {
@@ -93,17 +106,57 @@ fn part1(input: &str) {
                 machine
                     .buttons
                     .iter()
-                    .for_each(|&b| q.push_back((state ^ b, presses + 1)));
+                    .for_each(|b| q.push_back((state ^ b.value, presses + 1)));
             }
         })
         .sum::<u32>();
+
     println!("Day 10 Part 1: {}", sum);
 }
 
-fn _part2(input: &str) {
-    let _data = parse_input(input);
-    // TODO: solve part 2
-    println!("Day 10 Part 2: {}", "TODO");
+fn bump_counter(mut counter: Vec<u32>, idxs: &[u32]) -> Vec<u32> {
+    for &i in idxs {
+        unsafe {
+            *counter.get_unchecked_mut(i as usize) += 1;
+        }
+    }
+    counter
 }
 
-register_day!(2025, 10, part1);
+fn part2(input: &str) {
+    let machines = parse_input(input);
+
+    let sum = machines
+        .iter()
+        .progress()
+        .map(|machine| {
+            // state, presses
+            let mut q: VecDeque<(Vec<u32>, u32)> = machine
+                .buttons
+                .iter()
+                .map(|b| {
+                    (
+                        bump_counter(vec![0; machine.joltages.len()], &b.positions),
+                        1,
+                    )
+                })
+                .collect();
+            loop {
+                let (state, presses) = q.pop_front().unwrap();
+                if state == machine.joltages {
+                    return presses;
+                }
+                if state.iter().zip(&machine.joltages).any(|(a, b)| a > b) {
+                    continue;
+                }
+                machine.buttons.iter().for_each(|b| {
+                    q.push_back((bump_counter(state.clone(), &b.positions), presses + 1))
+                });
+            }
+        })
+        .sum::<u32>();
+
+    println!("Day 10 Part 2: {}", sum);
+}
+
+register_day!(2025, 10, part1, part2);
